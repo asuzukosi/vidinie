3188dcfd0cacc78b520a5bafd819eb2a3156aa6c
Ghoui <flareonverm@outlook.com>
2025-12-19 19:16:34 +0000

    changed the layout of the sidebar

--- frontend/app/(dashboard)/layout.tsx
+++ frontend/app/(dashboard)/layout.tsx
@@ -1,13 +1,13 @@
 import SidebarLayout from "@/components/Sidebar";
 
 export default function DashboardLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <div className="flex min-h-screen bg-zinc-50 font-sans dark:bg-black">
-            <SidebarLayout>{children}</SidebarLayout>
-        </div>
-    );
+  return (
+    <div className="flex min-h-screen bg-zinc-50 font-sans dark:bg-black">
+      <SidebarLayout>{children}</SidebarLayout>
+    </div>
+  );
 }
