3da05b2e84ed6eb7a069828d762304a5002a9bfa
asuzukosi <keloasuzu@yahoo.com>
2026-01-04 20:25:02 +0000

    [feat] remove trusted host authenticator on fastapi

--- api/core/config.py
+++ api/core/config.py
@@ -4,9 +4,7 @@ import os
 load_dotenv()
 
 MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb://localhost:27017")
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 settings = {
     "mongodb_uri": MONGODB_URI,
-    "allowed_hosts": ALLOWED_HOSTS
 }
